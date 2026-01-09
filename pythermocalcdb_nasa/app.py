14bb0a1fa638af7ab028d4c8c5926079e346e53e
sina.gilassi <sina.gilassi@gmail.com>
2026-01-09 13:39:07 -0500

    init

--- pythermocalcdb_nasa/app.py
+++ pythermocalcdb_nasa/app.py
@@ -1,5 +1,15 @@
 # import libs
 import logging
+from pythermodb_settings.models import Temperature, Component, CustomProp
+from pyThermoLinkDB.models import ModelSource
 
 # NOTE: set up logger
 logger = logging.getLogger(__name__)
+
+
+def calc_En(
+        component: Component,
+        temperature: Temperature,
+        model_source: ModelSource,
+):
+    pass
