81a05cd8632763775588cd542d345cce9e66b242
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-01-15 23:06:58 -0500

    Refactor NASA polynomial constants and temperature breaks for clarity and consistency

--- pythermocalcdb_nasa/configs/constants.py
+++ pythermocalcdb_nasa/configs/constants.py
@@ -12,22 +12,45 @@ T_ref_K = 298.15  # reference temperature in K
 P_ref_Pa = 101325.0  # reference pressure in Pa
 
 # SECTION: NASA polynomial types
-
-
 NASAType = Literal["nasa7", "nasa9"]
 
+# NASA polynomial temperature ranges
+NASA7_200_1000_K = "nasa7_200_1000_K"
+NASA7_1000_6000_K = "nasa7_1000_6000_K"
+NASA7_6000_20000_K = "nasa7_6000_20000_K"
+NASA9_200_1000_K = "nasa9_200_1000_K"
+NASA9_1000_6000_K = "nasa9_1000_6000_K"
+NASA9_6000_20000_K = "nasa9_6000_20000_K"
 
-NASA7_MIN = "nasa7_min"
-NASA7_MAX = "nasa7_max"
-NASA9_MIN = "nasa9_min"
-NASA9_MAX = "nasa9_max"
+NASARangeType = Literal[
+    "nasa7_200_1000_K",
+    "nasa7_1000_6000_K",
+    "nasa7_6000_20000_K",
+    "nasa9_200_1000_K",
+    "nasa9_1000_6000_K",
+    "nasa9_6000_20000_K",
+]
 
 NASA_POLY_TYPES = (
-    NASA7_MIN,
-    NASA7_MAX,
-    NASA9_MIN,
-    NASA9_MAX
+    NASA7_200_1000_K,
+    NASA7_1000_6000_K,
+    NASA7_6000_20000_K,
+    NASA9_200_1000_K,
+    NASA9_1000_6000_K,
+    NASA9_6000_20000_K,
 )
 
-TEMPERATURE_BREAK_NASA7_K = 1000.0  # temperature break for NASA7 polynomials in K
-TEMPERATURE_BREAK_NASA9_K = 1000.0  # temperature break for NASA7 polynomials in K
+# SECTION: Temperature breaks
+# NASA7
+# temperature break for NASA7 polynomials in K
+TEMPERATURE_BREAK_NASA7_200_K = 200.0
+# temperature break for NASA7 polynomials in K
+TEMPERATURE_BREAK_NASA7_1000_K = 1000.0
+# temperature break for NASA7 polynomials in K
+TEMPERATURE_BREAK_NASA7_6000_K = 6000.0
+# NASA9
+# temperature break for NASA9 polynomials in K
+TEMPERATURE_BREAK_NASA9_200_K = 200.0
+TEMPERATURE_BREAK_NASA9_1000_K = 1000.0  # temperature
+# temperature break for NASA9 polynomials in K
+TEMPERATURE_BREAK_NASA9_6000_K = 6000.0
