0e713c7cd739224a49f4e30c861489d65a2e483a
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2026-08-18 13:34:54 -0400

    feat: create __init__.py for reference module with builder imports

---
+++ pythermocalcdb_nasa/reference/__init__.py
@@ -0,0 +1,18 @@
+from .builder import (
+    REFERENCE_COLUMNS,
+    REFERENCE_TEMPLATE,
+    build_reference_from_database,
+    build_reference_from_rows,
+    database_row_to_reference_value,
+    format_reference_value,
+)
+
+
+__all__ = [
+    "REFERENCE_COLUMNS",
+    "REFERENCE_TEMPLATE",
+    "build_reference_from_database",
+    "build_reference_from_rows",
+    "database_row_to_reference_value",
+    "format_reference_value",
+]
